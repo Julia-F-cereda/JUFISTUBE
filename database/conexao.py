2c0e649fe494b5ca8d4806b111d0a55c6ee39b07
julia cereda <julia.cereda@aluno.senai.br>
2026-03-27 15:41:23 -0300

    COMITY:

--- database/conexao.py
+++ database/conexao.py
@@ -1,13 +1,26 @@
 import mysql.connector
 
+conexao_banco = "LOCAL"
+
 def conectar():
-        conexao = mysql.connector.connect(
-                host="localhost",
-                port=3306,
-                user="root",
-                password= "root",
-                database= "jufisTube"
-            )
+        if conexao_banco == "LOCAL":
+        
+            conexao = mysql.connector.connect(
+                    host="localhost",
+                    port=3306,
+                    user="root",
+                    password= "root",
+                    database= "jufisTube"
+                )
+        else:
+              conexao = mysql.connector.connect(
+                    host="server-juliafiscarellicereda-julia-fiscarelli-cereda.a.aivencloud.com",
+                    port=23810,
+                    user="23810",
+                    password= "AVNS_C-gZuerDKRNTQmnP_zp",
+                    database= "defaultdb"
+                )
+              
             #criando cursor
         cursor = conexao.cursor(dictionary=True)
 
